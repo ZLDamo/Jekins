31924614f157c313a880442fba91e4e1dbf565bb
Damo <ZLDamo@163.com>
2018-07-09 16:25:41 +0800

    init project

---
+++ Jekins/Jekins/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Jekins
+//
+//  Created by Damo on 2018/7/9.
+//  Copyright © 2018年 Damo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
--- TestDamo/Pods/Target Support Files/Pods-TestDamo/Pods-TestDamo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TestDamo : NSObject
-@end
-@implementation PodsDummy_Pods_TestDamo
-@end
